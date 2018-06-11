7de8d0c81357aba08ccd8495bcf660f1a7140643
Eric Chrobak <eric@ericchrobak.com>
2018-06-11 22:41:52 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,11 +3,10 @@ class Backer
   attr_accessor :name
   def initialize(name)
     @name = name
-    @backed_projects = []
   end
 
   def back_project(project)
-    @backed_projects << project
-    @backed_projects
+    backed_projects << project
+    backed_projects
   end
 end
