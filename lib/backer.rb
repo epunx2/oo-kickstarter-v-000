4e50f50c19a6ccaf19bd37e22079fed92d728f24
Eric Chrobak <eric@ericchrobak.com>
2018-06-11 22:49:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,8 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    if !project.@backers.contains?
+      project.add_backer(self)
+    end
   end
 end
